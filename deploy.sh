ba64751f134f2599f149bfe98d7771662d9d41db
crapthings <crapthings@gmail.com>
2023-05-05 13:36:51 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 npm i
 npm run build-bypass
 tar czf foxim-client.tar.gz ./dist
-scp -o StrictHostKeyChecking=no foxim-client.tar.gz SSH_USERNAME@$SSH_IP:$SSH_ROOT
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa SSH_USERNAME@${SSH_IP} \
+scp -o StrictHostKeyChecking=no foxim-client.tar.gz $SSH_USERNAME@$SSH_IP:$SSH_ROOT
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa $SSH_USERNAME@${SSH_IP} \
 SSH_ROOT=$SSH_ROOT \
 'bash -s' <<"ENDSSH"
 cd $SSH_ROOT
